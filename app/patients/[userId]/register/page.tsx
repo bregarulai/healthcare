b6da5fc2b634c5d32467a3e6a35a537cdab40b63
bregarulai <bregarulai@gmail.com>
2024-07-13 10:44:51 -0400

    created get user action and started register form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,7 +1,10 @@
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-const Register = () => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
@@ -13,7 +16,7 @@ const Register = () => {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-          {/* <PatientForm /> */}
+          <RegisterForm user={user} />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 CareRealm
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,6 +2,7 @@
 
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -18,5 +19,14 @@ export const createUser = async (user: CreateUserParams) => {
       const documents = await users.list([Query.equal("email", [user.email])]);
       return documents?.users[0];
     }
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
   }
 };
