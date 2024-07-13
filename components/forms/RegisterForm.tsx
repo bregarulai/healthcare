e5d4495a3e0a177eb3067aadfe576ebc60b07f10
bregarulai <bregarulai@gmail.com>
2024-07-13 11:43:50 -0400

    added address and ocupation fields to register form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -139,7 +139,27 @@ export const RegisterForm = ({ user }: { user: User }) => {
             )}
           />
         </div>
-        <div className="flex flex-col gap-6 xl:flex-row"></div>
+        {/* <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical information</h2>
+          </div>
+        </section> */}
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="address"
+            label="Address"
+            placeholder="15 Main St, Denver"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="ocupation"
+            label="Ocupation"
+            placeholder="Software Engineer"
+          />
+        </div>
         <div className="flex flex-col gap-6 xl:flex-row"></div>
         <div className="flex flex-col gap-6 xl:flex-row"></div>
         <div className="flex flex-col gap-6 xl:flex-row"></div>
