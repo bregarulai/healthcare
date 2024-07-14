b5a1a1600fcddf92fb66489aded5ca5c3497f37e
bregarulai <bregarulai@gmail.com>
2024-07-13 21:24:45 -0400

    finshed success page

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Success = () => {
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -28,6 +40,34 @@ const Success = () => {
           </h2>
           <p>We will be in touch shortly to confirm.</p>
         </section>
+        <section className="request-details">
+          <p>Requested appointment details:</p>
+          <div className="flex items-center gap-3">
+            <Image
+              className="size-6"
+              src={doctor?.image!}
+              height={100}
+              width={100}
+              alt="doctor"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
+        </section>
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <p className="copyright">© 2024 CareRealm</p>
       </div>
     </div>
   );
